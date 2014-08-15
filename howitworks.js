7bb8654bed3d576326de415818ca22b65644de38
Abhay Varshney <AbhayVarshney@Abhays-MacBook-Pro.local>
2014-08-14 22:32:18 -0700

    changes

--- howitworks.js
+++ howitworks.js
@@ -47,3 +47,20 @@ $(document).ready(function(){
 		});
 	};
 });
+
+// Scrolling feature
+$(document).ready(function () {
+	$(window).scroll(function () {
+	    if ($(this).scrollTop() > 100) {
+	        $('.scrollup').fadeIn();
+	    } else {
+	        $('.scrollup').fadeOut();
+	    }
+	});
+	$('.scrollup').click(function () {
+	    $("html, body").animate({
+	        scrollTop: 0
+	    }, 600);
+		return false;
+	});
+});
