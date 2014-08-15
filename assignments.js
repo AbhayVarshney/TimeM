60eabad27b8ed18d57e956116e5de326a7d55ef3
Varun Agarwal <varun.agarwal@gmail.com>
2014-08-15 09:29:30 -0700

    refactoring assignments

---
+++ assignments.js
@@ -0,0 +1,14 @@
+	
+
+
+	
+	
+
+	function changeThis() {
+        var assignmentInput = document.getElementById('assignment');
+        document.getElementById('newAssign').innerHTML = assignmentInput.value;
+        var minsInput = document.getElementById('mins');
+        document.getElementById('newMins').innerHTML = minsInput.value;
+     }
+
+   
