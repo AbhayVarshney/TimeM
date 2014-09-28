85ed6309fd94c26c5456f5c22d33126530e5e789
Abhay Varshney <AbhayVarshney@Abhays-MacBook-Pro.local>
2014-09-28 15:58:28 -0700

    changes

--- profile.js
+++ profile.js
@@ -1,20 +1,18 @@
-		
-
-
-	//Returns a printed date that is timeDistance from the current date. This function refers to times in the past
-	function currentDate(timeDistance){
-		var currentTime = new Date()
-		var month = currentTime.getMonth() + 1
-		var day = currentTime.getDate() - timeDistance;
-		var year = currentTime.getFullYear()
-		document.write(month + "/" + day + "/" + year)
-	}
 	
-	//Returns a printed date that is timeDistance from the current date. This function refers to times in the future
-	function futureDate(timeDistance){
-		var currentTime = new Date()
-		var month = currentTime.getMonth() + 1
-		var day = currentTime.getDate() + timeDistance;
-		var year = currentTime.getFullYear()
-		document.write(month + "/" + day + "/" + year)
-	}
+//Returns a printed date that is timeDistance from the current date. This function refers to times in the past
+function currentDate(timeDistance){
+	var currentTime = new Date()
+	var month = currentTime.getMonth() + 1
+	var day = currentTime.getDate() - timeDistance;
+	var year = currentTime.getFullYear()
+	document.write(month + "/" + day + "/" + year)
+}
+
+//Returns a printed date that is timeDistance from the current date. This function refers to times in the future
+function futureDate(timeDistance){
+	var currentTime = new Date()
+	var month = currentTime.getMonth() + 1
+	var day = currentTime.getDate() + timeDistance;
+	var year = currentTime.getFullYear()
+	document.write(month + "/" + day + "/" + year)
+}
